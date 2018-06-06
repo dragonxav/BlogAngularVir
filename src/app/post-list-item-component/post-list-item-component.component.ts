73bfcbbe19d3fa7aa99d6e9e8d7283bf2329368d
VirX <xavboxxavto@msn.com>
2018-06-06 10:38:59 +0200

    v1

---
+++ src/app/post-list-item-component/post-list-item-component.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item-component',
+  templateUrl: './post-list-item-component.component.html',
+  styleUrls: ['./post-list-item-component.component.scss']
+})
+export class PostListItemComponentComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() content: string;
+  @Input() loveIts: number;
+  @Input() created_at: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onUpLove() {
+    this.loveIts++;
+  }
+
+  onDownLove() {
+    this.loveIts--;
+  }
+
+}
